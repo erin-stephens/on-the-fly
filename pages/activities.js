6040faa728858f93a3df298c64b02dc760fedbad
Erin Stephens <erebstephens@gmail.com>
2023-02-28 18:35:38 -0600

    added button on activity page to link to form

--- pages/activities.js
+++ pages/activities.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import { getAllActivities } from '../API/activityData';
 import ActivityCard from '../components/ActivityCard';
 
@@ -19,7 +20,9 @@ export default function ActivitiesPage() {
       <Head>
         <title>All Activities</title>
       </Head>
-      <button type="button">Add an Activity</button>
+      <>
+        <Link passHref href="/activity/new"><button type="button">Add an Activity</button></Link>
+      </>
       <h1> All Activities </h1>
       <div className="d-flex flex-wrap">
         {activities.map((activity) => (
