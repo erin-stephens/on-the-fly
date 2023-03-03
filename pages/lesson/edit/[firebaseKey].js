b3fdfe88aade46b63a51f20b1802f3b51da910d7
Erin Stephens <erebstephens@gmail.com>
2023-03-02 19:45:32 -0600

    added edit functionality to both activity and lesson cards

--- pages/lesson/edit/[firebaseKey].js
+++ pages/lesson/edit/[firebaseKey].js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { getSingleLesson } from '../../../api/lessonData';
+import LessonForm from '../../../components/forms/LessonForm';
 
 export default function EditLessonPlan() {
+  const [editLesson, setEditLesson] = useState({});
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    getSingleLesson(firebaseKey).then(setEditLesson);
+  }, [firebaseKey]);
+
   return (
-    <div>EditLessonPlan</div>
+    <LessonForm obj={editLesson} />
   );
 }
