951b03fd6d15688007234dad45e4c3ab7f305222
Erin Stephens <erebstephens@gmail.com>
2023-03-07 19:15:46 -0600

    created user page that only shows activities/lessons created by the user, tested with separate login to confirm

---
+++ components/userpage/UserLessonCarousel.js
@@ -0,0 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
+import { Carousel } from 'react-bootstrap';
+import LessonCard from '../LessonCard';
+import { getUserLessons } from '../../api/lessonData';
+import { useAuth } from '../../utils/context/authContext';
+
+export default function UserLessonCarousel() {
+  const [lessons, setLessons] = useState([]);
+  const { user } = useAuth();
+
+  const getLessons = () => {
+    getUserLessons(user.uid).then(setLessons);
+  };
+  useEffect(() => {
+    getLessons();
+  }, [user]);
+  return (
+    <Carousel interval={null} variant="dark" className="lessonCarouselContainer">
+      {lessons.map((lesson) => (
+        <Carousel.Item className="lessonCarouselItem">
+          <LessonCard key={lesson.firebasekey} lessonObj={lesson} onUpdate={getLessons} />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+}
--- pages/myPage.js
+++ pages/myPage.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import UserActivityCarousel from '../components/userpage/UserActivityCarousel';
+import UserLessonCarousel from '../components/userpage/UserLessonCarousel';
 
-export default function myPage() {
+export default function Home() {
   return (
-    <div>my-page</div>
+    <>
+      <div className="activityIndex">
+        <h1>All Activities</h1>
+        <div className="activityCarouselContainer">
+          <UserActivityCarousel />
+        </div>
+      </div>
+      <hr />
+      <div>
+        <h1>All Lesson Plans</h1>
+        <div className="lessonCarouselContainer">
+          <UserLessonCarousel />
+        </div>
+      </div>
+    </>
   );
 }
