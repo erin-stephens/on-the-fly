dcde0e5c8af5269fea19598867c5d711ec217ae8
Erin Stephens <erebstephens@gmail.com>
2023-03-15 08:48:19 -0500

    added comment form, cards, api calls, divs in activities

---
+++ components/CommentCard.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Card, Button } from 'react-bootstrap';
+import { useAuth } from '../utils/context/authContext';
+import { deleteComment } from '../api/commentData';
+import { getSingleActivity } from '../api/activityData';
+
+export default function CommentCard({ commentObj, onUpdate }) {
+  const deleteThisComment = () => {
+    if (window.confirm('Delete your comment?')) {
+      deleteComment(commentObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  const { user } = useAuth();
+  const [activity, setActivity] = useState({});
+
+  useEffect(() => {
+    getSingleActivity(commentObj.activity_id).then(setActivity);
+  }, [commentObj.activity_id]);
+
+  return (
+    <div>
+      <Card className="comment-card">
+        <div className="comment-container">
+          <Card.Header>{commentObj.date_added}</Card.Header>
+          <Card.Body>
+            <blockquote className="blockquote mb-0">
+              <p>
+                {' '}
+                {commentObj.text}
+                {' '}
+              </p>
+              <footer className="blockquote-footer">
+                {commentObj.author}
+                {commentObj.uid === user.uid || user.displayName === activity.username
+                  ? (
+                    <Button
+                      className="red-btn comment-btn"
+                      onClick={deleteThisComment}
+                    >
+                      Delete
+                    </Button>
+                  ) : ''}
+              </footer>
+            </blockquote>
+
+          </Card.Body>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+CommentCard.propTypes = {
+  commentObj: PropTypes.shape({
+    activity_id: PropTypes.string,
+    firebaseKey: PropTypes.string,
+    text: PropTypes.string,
+    date_added: PropTypes.string,
+    author: PropTypes.string,
+    uid: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
---
+++ components/forms/CommentForm.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Form, FloatingLabel, Button } from 'react-bootstrap';
+import { PropTypes } from 'prop-types';
+import { useAuth } from '../../utils/context/authContext';
+import { createComment, updateComment } from '../../api/commentData';
+
+const initialState = {
+  firebaseKey: '',
+  uid: '',
+  text: '',
+  activity_id: '',
+  date_added: '',
+  author: '',
+};
+
+export default function CommentForm({ activityFbKey, onUpdate }) {
+  const [formInput, setFormInput] = useState(initialState);
+  const { user } = useAuth();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      ...formInput,
+      uid: user.uid,
+      date_added: new Date().toLocaleString(),
+      author: user.displayName,
+      activity_id: activityFbKey,
+    };
+    createComment(payload).then(({ name }) => {
+      const patchPayload = { firebaseKey: name };
+      updateComment(patchPayload)
+        .then(() => onUpdate())
+        .then(setFormInput(initialState));
+    });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h4 className="mt-3 mb-3">Add A Comment</h4>
+
+      <FloatingLabel controlId="floatingTextArea" label="Type your comment here..." className="mb-3 text-black">
+        <Form.Control
+          type="textarea"
+          style={{ height: '100px' }}
+          name="text"
+          value={formInput.text}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      <Button type="submit" className="redBtn">Submit Comment</Button>
+    </Form>
+  );
+}
+
+CommentForm.propTypes = {
+  activityFbKey: PropTypes.string.isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
